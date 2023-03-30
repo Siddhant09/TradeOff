f5d8bc0353f177f6f59ad5e80109488d47aede63
Siddhant09 <siddhant.c09@gmail.com>
2023-03-30 08:49:59 -0400

    ui changes

--- TradeOff/App.xaml.cs
+++ TradeOff/App.xaml.cs
@@ -6,5 +6,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
         MainPage = new AppShell();
-	}
+        Application.Current.UserAppTheme = AppTheme.Dark;
+    }
 }
--- TradeOff/Views/SettingsPage.xaml.cs
+++ TradeOff/Views/SettingsPage.xaml.cs
@@ -5,5 +5,21 @@ public partial class SettingsPage : ContentPage
 	public SettingsPage()
 	{
 		InitializeComponent();
-	}
+
+        if(Application.Current.UserAppTheme == AppTheme.Dark)
+            ddlTheme.SelectedIndex = 0;
+        else if (Application.Current.UserAppTheme == AppTheme.Light)
+            ddlTheme.SelectedIndex = 1;
+    }
+
+    private void ddlTheme_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+
+        if (selectedIndex == 0)
+            Application.Current.UserAppTheme = AppTheme.Dark;
+        else if (selectedIndex == 1)
+            Application.Current.UserAppTheme = AppTheme.Light;
+    }
 }
